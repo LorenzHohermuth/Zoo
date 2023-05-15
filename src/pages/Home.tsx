aaa84a0057c95ad976af50b84a4094d0876a38f3
Lorenz Hohermuth <lofloho@gmail.com>
2023-05-15 10:58:35 +0200

    testing pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <img src="./Images/indischer-tiger.jpg" alt="tieger-wwf" className='w-screen object-cover md:h-250 h-144'/>
+        <img src="https://eu2.contabostorage.com/45e93289c054458996e4f2c13ea273c4:lennylam/indischer-tiger.png" alt="tieger-wwf" className='w-screen object-cover md:h-250 h-144'/>
       </div>
 
       
